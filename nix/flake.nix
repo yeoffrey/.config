bf3e9decb8aeea8c5003018a4c6530a3447e0540
Geoffrey Belcher <geoffreybelcher@icloud.com>
2024-11-29 13:35:48 -0330

    Remove themes directory

--- nix/flake.nix
+++ nix/flake.nix
@@ -23,34 +23,38 @@
 
   outputs = { self, nixpkgs, home-manager, darwin, homebrew }@inputs:
     let
+      inherit (self) outputs;
+
+      # Define user configurations
+      users = {
+        geoff = {
+          email = "geoffreybelcher@icloud.com";
+          fullName = "Geoffrey Belcher";
+          name = "geoff";
+        };
+      };
+
       # Function for nix-darwin system configuration
       mkDarwinConfiguration = hostname: username:
         darwin.lib.darwinSystem {
+          specialArgs = {
+            inherit inputs outputs hostname;
+            userConfig = users.${username};
+          };
           modules = [
             home-manager.darwinModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.geoff = import ./users/${username}.nix;
+              };
+            }
             homebrew.darwinModules.nix-homebrew
             ./hosts/${hostname}/configuration.nix
-            ./users/${username}.nix
           ];
         };
     in {
-      darwinConfigurations."air" = darwin.lib.darwinSystem {
-        modules = [
-          home-manager.darwinModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.geoff = import ./users/geoff.nix;
-            };
-          }
-          home-manager.darwinModules.home-manager
-          homebrew.darwinModules.nix-homebrew
-          ./hosts/air/configuration.nix
-        ];
-      };
-
-      # Expose the package set, including overlays, for convenience.
-      darwinPackages = self.darwinConfigurations."air".pkgs;
+      darwinConfigurations = { "air" = mkDarwinConfiguration "air" "geoff"; };
     };
 }
