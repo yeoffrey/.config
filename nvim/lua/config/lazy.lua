c38d45e647d045d2a7dd834caff9c9c62ebe3a1d
Geoffrey Belcher <geoffreybelcher@icloud.com>
2024-12-01 17:42:26 -0330

    Neovim: Remove custom Biome configuration for the packaged one

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.leap" },
+    { import = "lazyvim.plugins.extras.formatting.biome" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.git" },
--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,43 +1,9 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shfmt",
-        "biome",
-      },
-    },
-  },
-  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        ts = { "biome" },
-        json = { "biome" },
         rs = { "rustfmt" },
-      },
-      formatters = {
-        biome = {
-          condition = function(ctx)
-            return vim.fs.find({ "biome.json" }, { path = ctx.filename, upward = true })[1]
-          end,
-        },
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        ts = { "biome", "eslint" },
-      },
-      linters = {
-        biome = {
-          condition = function(ctx)
-            return vim.fs.find({ "biome.json" }, { path = ctx.filename, upward = true })[1]
-          end,
-        },
       },
     },
   },
