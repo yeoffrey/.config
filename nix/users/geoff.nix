bac303ddc5efe22dda799556638e5327a784da22
Geoffrey Belcher <geoffreybelcher@icloud.com>
2024-12-02 16:41:01 -0330

    Nix: Enable oh-my-zsh

--- nix/users/geoff.nix
+++ nix/users/geoff.nix
@@ -30,6 +30,7 @@
 
     zsh = {
       enable = true;
+      oh-my-zsh = { enable = true; };
       enableCompletion = true;
       autosuggestion = { enable = true; };
       syntaxHighlighting.enable = true;
