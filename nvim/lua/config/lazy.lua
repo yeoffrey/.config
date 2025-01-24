75d03c40cce9a919484577c970b50ef2e192537c
Geoffrey Belcher <geoffreybelcher@icloud.com>
2025-01-24 11:35:24 -0330

    Neovim: Add svelte

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
