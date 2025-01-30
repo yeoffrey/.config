080c8978b380a8d5a2f1be4f40bc3e9ab0b73d37
Geoffrey Belcher <geoffreybelcher@icloud.com>
2025-01-30 12:43:22 -0330

    neovim: Add Tailwindcss support

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
