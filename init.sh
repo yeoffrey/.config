ee9cfd3837fa0be3dd7b85f1b056c0571ada6b72
Geoffrey Belcher <geoffreybelcher@Geoffreys-Mac-mini.local>
2025-03-06 11:07:21 -0500

    Remove useless UI feedback on init script

--- init.sh
+++ init.sh
@@ -33,8 +33,6 @@ echo ".tmux.conf symlink created successfully."
 if ! command -v brew &>/dev/null; then
   echo "Homebrew not found. Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-  echo "Homebrew is already installed."
 fi
 
 # Update Homebrew
